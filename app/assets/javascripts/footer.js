b0fe00951f666bfa4d270c9a4bcb38fde54ed8b2
Scott McElveen <scott.mcelveen@hotmail.com>
2016-03-13 15:21:25 +0000

    Bring in bootstrap to the layouts etc

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -10,4 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
+//= require jquery/dist/jquery
+//= require bootstrap/dist/js/bootstrap
